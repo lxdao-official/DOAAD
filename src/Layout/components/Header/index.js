aa4ed1d9c6f3a741a1052b679e6ae117319c313f
zon <zqzhong1211@163.com>
2023-01-26 23:05:12 +0800

    feat: 调整样式

--- src/Layout/components/Header/index.js
+++ src/Layout/components/Header/index.js
@@ -4,7 +4,10 @@ import './index.css'
 export default function Header() {
   return (
     <div className='doadd-header'>
-
+      <div className='header-content'>
+        <imgc className="logo" alt='logo' />
+        <span>DOADD</span>
+      </div>
     </div>
   );
 }
