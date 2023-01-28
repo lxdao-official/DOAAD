4693073efc7ac2c8fcdc1b91e555380deb42bb9c
zon <zqzhong1211@163.com>
2023-01-28 16:28:40 +0800

    feat: 布局调整

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import logo from '../../assets/logo.png';
 import './index.less';
 const Header = () => (
-  <div className="header-eth-operation">
-    <div className='operation left-container'>
+  <div className='header-wrapper'>
+    <div className="header-eth-operation">
+      <div className="container-logo-operation">
+        <a href='/'>
+          <img className='logo' src={logo} />
+        </a>
+        <div className='operation left-container'>
 
-    </div>
-    <div className='operation right-container'>
+        </div>
+      </div>
+      <div className='operation right-container'>
 
+      </div>
     </div>
   </div>
 );
